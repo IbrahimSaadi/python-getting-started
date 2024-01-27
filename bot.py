59e26d79b3aee05150e4b15cab5958aa39074138
IbrahimSaadi <109934496+IbrahimSaadi@users.noreply.github.com>
2024-01-27 12:49:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,42 +1,9 @@
-import os
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from bardapi import Bard
-
-# Set your Bard API key
-os.environ["_BARD_API_KEY"] = "fgiipqRVVvDXgwesAW08bCv_CT0Ii68qXGsU3alb1dka6aWBG4OzFqmKxLsVkDtZBbatgw."
-
-# Define the function to handle incoming messages
+from pytesseract import image_to_string
+from PIL import Image
+import pytesseract
+# your path may be different
+#pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 
-def handle_message(update: Update, context: CallbackContext) -> None:
-    message_text = update.message.text
-    response = Bard().get_answer(str(message_text))['content']
-    update.message.reply_text(response)
-
-
-def main():
-    # Set your Telegram Bot token
-    TELEGRAM_TOKEN = '5988601506:AAH_t0h3R7i7KBOHgbOHcVwnvyliKgf16c4'
-
-    # Create the Updater and pass it your bot's token
-    updater = Updater(TELEGRAM_TOKEN)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # Register the command handler for /start command
-
-    # Register the message handler to handle all messages
-    dp.add_handler(MessageHandler(Filters.text & ~
-                   Filters.command, handle_message))
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you send a signal to stop it
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+print(pytesseract.image_to_string(
+    'image.png', lang='ara'))
